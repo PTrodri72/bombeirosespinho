c06eecf5ccffe66c4a1080dafbbcf26f5c033d1c
PTrodri72 <rodrigofilipe631@gmail.com>
2026-03-30 23:24:20 +0100

    Update dados.py

--- dados.py
+++ dados.py
@@ -1,4 +1,5 @@
-# Ficheiro de Armazenamento Permanente
+# Ficheiro de Armazenamento Permanente - BV ESPINHO
+
 users = {
     "igor.rodrigues@comandobombeiros.galiarp.pt": {
         "nome": "Igor Rodrigues",
@@ -7,4 +8,17 @@ users = {
     }
 }
 
-stats = {"incendios": 124}
+# Adicionei 'socorros' e 'operacionais' para preencher o site todo
+stats = {
+    "incendios": 124,
+    "socorros": 315,
+    "operacionais": 24
+}
+
+# Adicionei esta lista para a aba "Parque Auto" não aparecer vazia
+viaturas = [
+    {"id": "VUCI-01", "tipo": "Combate Urbano", "estado": "Operacional"},
+    {"id": "ABSC-01", "tipo": "Socorro", "estado": "Operacional"},
+    {"id": "VCOT-01", "tipo": "Comando", "estado": "Operacional"},
+    {"id": "VFGC-02", "tipo": "Combate Florestal", "estado": "Oficina"}
+]
