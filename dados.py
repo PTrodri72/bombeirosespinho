65b5210429c9a878c319cc5565e09f13686a3a2c
PTrodri72 <rodrigofilipe631@gmail.com>
2026-03-30 23:13:48 +0100

    Add permanent storage file for users and stats

---
+++ dados.py
@@ -0,0 +1,10 @@
+# Ficheiro de Armazenamento Permanente
+users = {
+    "igor.rodrigues@comandobombeiros.galiarp.pt": {
+        "nome": "Igor Rodrigues",
+        "senha": "GRPGALAIMELHORSERVIDOR",
+        "patente": "Comandante"
+    }
+}
+
+stats = {"incendios": 124}
